1716c664b776d08968cef26a75eb8126fa2fdabf
StGrv <65493257+StGrv@users.noreply.github.com>
2022-06-10 18:23:28 +0300

    PhP function that receives the AJAX data

---
+++ AjaxController.php
@@ -0,0 +1,13 @@
+public function actionRemoveshopdelivery() {
+    $id = intval( yii::$app->request->post( 'id' ) );
+
+    $msg = "The delivery session does not exist or the ID is wrong";
+
+    if ( $id === 1 && isset( $_SESSION['delivery'] ) ) {
+        unset($_SESSION['delivery']);
+        $msg = "Success";
+    }
+
+    $result['message'] = $msg;
+    return json_encode( $result );
+}
